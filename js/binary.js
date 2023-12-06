2db9b293317828dd6d33910041218c573cbe6821
Alan Bui <alanb3350@gmail.com>
2023-12-05 21:58:15 -0500

    made 0s and 1s go full screen

--- js/binary.js
+++ js/binary.js
@@ -5,25 +5,22 @@ c.height = window.innerHeight;
 c.width = window.innerWidth;
 
 var matrix = [
-    "1 1 1 1 1 1 1 1 1 1 1 ",
-    "0 1 1 1 1 1 1 0 1 1 1 ",
-    "0 0 1 0 1 0 0 0 0 0 1 ",
-    "1 0 0 0 0 1 1 0 0 1 0 ",
-    "1 0 0 0 1 0 0 0 0 0 0 ",
-    "0 0 1 1 0 0 0 1 1 1 1 "
+    "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 ",
+    "0 1 1 1 1 1 1 0 1 1 1 0 1 1 1 1 1 1 0 1 1 1 1 1 1 0 1 1 1 ",
+    "0 0 1 0 1 0 0 0 0 0 1 0 0 1 0 1 0 0 0 0 0 1 1 0 0 0 0 0 1 ",
+    "1 0 0 0 0 1 1 0 0 1 0 1 0 0 0 0 1 1 0 0 1 0 0 1 1 0 0 1 0 ",
+    "1 0 0 0 1 0 0 0 0 0 0 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 ",
+    "0 0 1 1 0 0 0 1 1 1 1 0 0 1 1 0 0 0 1 1 1 1 0 0 0 1 1 1 1 "
 ];
 
-var font_size = 20;
+var font_size = 25;
 var drops = [];
 
 for (var x = 0; x < matrix[0].length; x++) {
-    drops[x] = c.width - (x * font_size) + 300;
+    drops[x] = (x * font_size) ;
 }
 
 function draw() {
-    ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
-    ctx.fillRect(0, 0, c.width, c.height);
-
     for (var i = 0; i < drops.length; i++) {
         var rowIndex = drops[i] % matrix.length;
         var text = matrix[rowIndex][i];
@@ -31,11 +28,12 @@ function draw() {
         // Stop raining when drops reach a certain point on the screen
         if (drops[i] * font_size <= c.height) {
             ctx.fillStyle = "#704292";
+            // ctx.fillStyle = "#ffffff"
             ctx.font = font_size + "px arial";
             ctx.fillText(text, c.width - i * font_size, drops[i] * font_size);
         }
 
-        if (drops[i] * font_size > c.height && Math.random() > 0.975) {
+        if (drops[i] * font_size > c.height && Math.random() > 0.8) {
             drops[i] = 0;
         }
 
@@ -43,43 +41,14 @@ function draw() {
     }
 }
 
-setInterval(draw, 3);
+setInterval(draw, 50);
 
-var leftMatrix = [
-    "1 1 1 1 1 1 1 1 1 1 1 ",
-    "0 1 1 1 1 1 1 0 1 1 1 ",
-    "0 0 1 0 1 0 0 0 0 0 1 ",
-    "1 0 0 0 0 1 1 0 0 1 0 ",
-    "1 0 0 0 1 0 0 0 0 0 0 ",
-    "0 0 1 1 0 0 0 1 1 1 1 "
-];
-
-var leftDrops = [];
-
-for (var x = 0; x < leftMatrix[0].length; x++) {
-    leftDrops[x] = c.width - (x * font_size) - 20; 
+var backgroundImage = new Image();
+backgroundImage.src = 'assets/bg2.svg'; 
+function drawBG() {
+    // ctx.fillStyle = "rgba(0, 0, 0, 1)";
+    // ctx.fillRect(0, 0, c.width, c.height);
+    ctx.drawImage(backgroundImage, 0, 0, c.width, c.height);
 }
 
-function drawLeft() {
-    ctx.fillStyle = "rgba(0, 0, 0, -1000)";
-    ctx.fillRect(0, 0, c.width, c.height);
-
-    for (var i = 0; i < leftDrops.length; i++) {
-        var rowIndex = leftDrops[i] % leftMatrix.length;
-        var text = leftMatrix[rowIndex][i];
-
-        // Stop raining when drops reach a certain point on the screen
-        if (leftDrops[i] * font_size >= c.height) {
-            ctx.fillStyle = "#414291";
-            ctx.font = font_size + "px arial";
-            ctx.fillText(text, i * font_size, leftDrops[i] * font_size);
-        }
-
-        if (leftDrops[i] * font_size > c.height && Math.random() > 0.975) {
-            leftDrops[i] = 0;
-        }
-
-        leftDrops[i]++;
-    }
-}
-setInterval(drawLeft,1);
+setInterval(drawBG, 30); 
