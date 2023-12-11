5f0e8c3a023ca72dfdcea342872e8a0d43b634e0
Alan Bui <alanb3350@gmail.com>
2023-12-11 18:38:24 -0500

    new background

--- js/binary.js
+++ js/binary.js
@@ -4,7 +4,7 @@ var ctx = c.getContext("2d");
 c.height = window.innerHeight;
 c.width = window.innerWidth;
 
-var font_size = 10;
+var font_size = 14;
 var columns = c.width / font_size;
 var drops = [];
 
@@ -12,10 +12,16 @@ for (var x = 0; x < columns; x++) {
   drops[x] = 1;
 }
 
+function randColor() {
+    var colors = ['rgba(121, 17, 246, 0.6)', 'rgba(221, 70, 215, 0.6)', 'rgba(9, 201, 209, 0.6)'];
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+
 function draw() {
-  ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
-  ctx.fillRect(0, 0, c.width, c.height);
-  ctx.fillStyle = getRandomColor();
+  drawBG()
+    // ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    // ctx.fillRect(0, 0, c.width, c.height);
+  ctx.fillStyle = randColor();
   ctx.font = font_size + "px arial";
 
   for (var i = 0; i < drops.length; i++) {
@@ -44,3 +50,10 @@ function getRandomColor() {
 
 
 
+var backgroundImage = new Image();
+backgroundImage.src = 'assets/bg4.svg'; 
+function drawBG() {
+    ctx.drawImage(backgroundImage, 0, 0, c.width, c.height);
+}
+
+// setInterval(drawBG, 3000);
